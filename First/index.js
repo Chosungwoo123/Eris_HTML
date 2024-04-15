0dbbb1f653c391065e72aaec55a66e75871e1bb2
Chosungwoo123 <ejyoo12345@naver.com>
2024-04-15 14:03:04 +0900

    Add Shop Image

--- First/index.js
+++ First/index.js
@@ -14,6 +14,16 @@ const background = new Sprite({
     imageSrc : "/First/img/background.png",
 })
 
+const shop = new Sprite({
+    position : {
+        x : 600,
+        y : 128,
+    },
+    imageSrc : "/First/img/shop.png",
+    scale : 2.75,
+    framesMax : 6,
+})
+
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 const player = new Fighter({
@@ -93,6 +103,7 @@ function animate() {
     c.fillRect(0,0,canvas.width,canvas.height);
 
     background.update();
+    shop.update();
     player.update();
     enemy.update();
 
